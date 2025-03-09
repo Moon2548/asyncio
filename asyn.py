68b5ea199314c170e078cccd20727099c1888a65
Taveeporn Chuaybumrung <taweeporn16078@gmail.com>
2025-03-09 20:40:09 +0700

    create func fetch

---
+++ asyn.py
@@ -0,0 +1,16 @@
+import asyncio
+import aiohttp
+import time
+
+URLS = [
+    "https://jsonplaceholder.typicode.com/posts/1",
+    "https://jsonplaceholder.typicode.com/posts/2",
+    "https://jsonplaceholder.typicode.com/posts/3",
+    "https://jsonplaceholder.typicode.com/posts/4",
+    "https://jsonplaceholder.typicode.com/posts/5",
+]
+
+
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.json()
